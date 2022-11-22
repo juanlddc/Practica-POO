28ecb293afa1c480fe79fbbd06063ae64cf001a0
Juan Luis <juanl.diazdelcastillo@gmail.com>
2022-11-22 15:58:35 +0100

    Comprobar Victoria V y H, no perfecto

--- src/Victoria.java
+++ src/Victoria.java
@@ -19,7 +19,7 @@ public class Victoria {
 
     public boolean haGanado(Tablero tablero, Turno turno) {
         haGanadoVert(tablero, turno);
-        haGanadoHor(tablero, turno);
+        //haGanadoHor(tablero, turno);
         return victoria;
     }
 
@@ -27,36 +27,38 @@ public class Victoria {
         String[] fichas = new String[3];
         for(int i = 0; i < COL; i++){
             for(int j = 0; j < FIL; j++){
-                for(int z = 0; z < 3; z++){
-                    fichas[i+1] = fichas[i];
-                }
-                fichas[0] = tablero.getPosicion(i,j);
-                victoria = comprobarArray(fichas);
+                actualizarArray(fichas, turno.getFichas());
+                fichas[0] = turno.getFichas();
+                victoria = comprobarArray(fichas, turno.getFichas());
             }
         }
     }
 
-    private void haGanadoHor(Tablero tablero, Turno turno) {
-        String[] fichas = new String[3];
+    /*private void haGanadoHor(Tablero tablero, Turno turno) {
+        String[] fichas = new String[4];
         for(int i = 0; i < FIL; i++){
             for(int j = 0; j < COL; j++){
-                for(int z = 0; z < 3; z++){
-                    fichas[i+1] = fichas[i];
-                }
-                fichas[0] = tablero.getPosicion(i,j);
-                victoria = comprobarArray(fichas);
+                actualizarArray(fichas, turno.getFichas());
+                fichas[0] = turno.getFichas();
+                victoria = comprobarArray(fichas, turno.getFichas());
             }
         }
-    }
+    }*/
 
-    private boolean comprobarArray(String[] fichas) {
+    private boolean comprobarArray(String[] fichas, String ficha) {
         boolean iguales = true;
 
-        for(int i = 0; i < 3; i ++){
-            if(!Objects.equals(fichas[i], fichas[i + 1])){ //fichas[i] != fichas[i+1]
+        for(int i = 0; i < fichas.length-1; i ++){
+            if(!Objects.equals(fichas[i], ficha)){ //fichas[i] != ficha
                 iguales = false;
             }
         }
         return iguales;
+    }
+
+    private void actualizarArray(String[] fichas, String ficha) {
+        for(int i = 0; i < fichas.length-1; i ++){
+            fichas[i+1] = fichas[i];
+        }
     }
 }
